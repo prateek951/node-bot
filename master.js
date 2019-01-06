510939280186f28accde2d8fd5ed95b3ad545dca
prateek951 <prince.cenation@gmail.com>
2019-01-06 22:41:25 +0530

    added session storage and history command

--- master.js
+++ master.js
@@ -1,28 +1,86 @@
 require("dotenv").config();
-const TELEGRAF = require("telegraf");
-const bot = new TELEGRAF(process.env.TELEGRAM_BOT_TOKEN);
 const AXIOS_CLIENT = require("axios");
+const TELEGRAF = require("telegraf");
+const LocalSession = require("telegraf-session-local");
+const bot = new TELEGRAF(process.env.TELEGRAM_BOT_TOKEN);
+
+bot.use(new LocalSession({ database: ".data/session.json" }).middleware());
 // bot.start(ctx => ctx.reply('Welcome'));
 bot.start(({ reply }) => reply("Welcome"));
 
-bot.startPolling();
-bot.help(({ reply }) => reply("How can I help you ! Feel free to ask!"));
+// bot.help(({ reply }) => reply("How can I help you ! Feel free to ask!"));
 
 //client reply
 // listen to incoming message
 // bot.on("message", ({ reply }) => reply("Hey!"));
 // bot.hears("hi", ({ reply }) => reply("Hi there"));
+bot.command("from", ({ message, reply, session }) => {
+  const lang = message.text.substring(6);
+  const { length: LNG_CODE_LENGTH } = lang;
+  if (LNG_CODE_LENGTH > 2 || LNG_CODE_LENGTH === 1) {
+    reply("🤔 Language code must be 2 characters, e.g. 'en' or 'fr'");
+    return;
+  }
+  reply(
+    lang
+      ? `✅ "from" language set to ${lang} `
+      : `✅ autodetect "from" language`
+  );
+  //Set the session from property
+  session.from = lang;
+});
 
-bot.on("message", async ({ message, reply }) => {
+bot.command("to", ({ message, reply, session }) => {
+  const lang = message.text.substring(4);
+  const { length: LNG_CODE_LENGTH } = lang;
+  if (LNG_CODE_LENGTH === 0) {
+    reply(
+      '🤔 Please specify a language code! It must be 2 chars, e.g. "en" or "fr"'
+    );
+    return;
+  }
+  if (LNG_CODE_LENGTH > 2 || LNG_CODE_LENGTH === 1) {
+    reply('🤔 Language code must be 2 chars, e.g. "en" or "fr"');
+    return;
+  }
+  //Set the session to property
+  session.to = lang;
+  reply(`✅ "to" language set to ${lang} `);
+});
+
+bot.on("message", async ({ message, reply, session }) => {
+  const lang = (session.from ? `${session.from}-` : ``) + (session.to || "en");
+  console.log(lang);
   const response = await AXIOS_CLIENT.get(
     "https://translate.yandex.net/api/v1.5/tr.json/translate",
     {
       params: {
         key: process.env.YANDEX_API_KEY,
         text: message.text,
-        lang: "en"
+        lang: lang
       }
     }
   );
-  reply(response.data.text[0]);
+  const translation = response.data.text[0];
+  reply(translation);
+
+  let messages = JSON.parse(session.messages) || [];
+  messages.push({ text: message.text, translation: translation });
+  session.messages = JSON.stringify(messages);
 });
+
+bot.command("history", ({ reply }) => {
+  try {
+    reply(
+      JSON.parse(session.messages)
+        .map(message => `${message.text} : ${message.translation}`)
+        .join("\n")
+    );
+  } catch (ex) {
+    const error = new Error(ex);
+    error.statusCode = 500;
+    throw error;
+  }
+});
+
+bot.startPolling();
