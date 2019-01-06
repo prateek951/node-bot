7727c02bba200804fef3759ae0b7603122620d5f
prateek951 <prince.cenation@gmail.com>
2019-01-06 21:33:52 +0530

    translation code works for the bot, added axios

--- master.js
+++ master.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const TELEGRAF = require("telegraf");
 const bot = new TELEGRAF(process.env.TELEGRAM_BOT_TOKEN);
-
+const AXIOS_CLIENT = require("axios");
 // bot.start(ctx => ctx.reply('Welcome'));
 bot.start(({ reply }) => reply("Welcome"));
 
@@ -10,5 +10,19 @@ bot.help(({ reply }) => reply("How can I help you ! Feel free to ask!"));
 
 //client reply
 // listen to incoming message
-bot.on("message", ({ reply }) => reply("Hey!"));
-bot.hears("hi", ({ reply }) => reply("Hi there"));
+// bot.on("message", ({ reply }) => reply("Hey!"));
+// bot.hears("hi", ({ reply }) => reply("Hi there"));
+
+bot.on("message", async ({ message, reply }) => {
+  const response = await AXIOS_CLIENT.get(
+    "https://translate.yandex.net/api/v1.5/tr.json/translate",
+    {
+      params: {
+        key: process.env.YANDEX_API_KEY,
+        text: message.text,
+        lang: "en"
+      }
+    }
+  );
+  reply(response.data.text[0]);
+});
